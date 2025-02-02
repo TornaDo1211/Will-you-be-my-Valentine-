c18a96ff6132ac273432a22ad63c233ac2c5a1f0
TornaDo1211 <ilayk06@gmail.com>
2025-02-02 23:47:50 +0200

    change it

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
+    "I will kill my self!",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
